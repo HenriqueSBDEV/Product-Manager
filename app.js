dac3834f38bbd221658210c66e83c68f18dfff34
Henrique Sousa Brito <henriquesousa5507@gmail.com>
2024-09-09 20:12:15 -0300

    Adicionando cadastro de produtos

--- app.js
+++ app.js
@@ -3,11 +3,23 @@ const mysql = require('mysql2');
 const app = express();
 const port = 3000;
 const { engine } = require('express-handlebars');
+const fileUpload = require('express-fileupload');
+app.use('/bootstrap', express.static('./node_modules/bootstrap/dist'));
+
+//Adicionar css
+app.use('/css', express.static('./css'));
+
+//Habilitando o fileUpload de arquivos
+app.use(fileUpload());
 
 //Configuracao handle-bars
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 app.set('views', './views');
+
+//Manipulação de dados via rotas
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
 
 //Configuração de conexão
 const conexao = mysql.createConnection({
@@ -26,6 +38,26 @@ conexao.connect(function(erro){
 //Rota principal
 app.get("/", function(req, res){
   res.render('formulario');
+})
+
+//Rota de cadastro
+app.post('/cadastrar', function(req, res){
+  let nome = req.body.nome;
+  let valor = req.body.valor;
+  let imagem = req.files.imagem.name;
+
+  let sql = `INSERT INTO produtos (nome, valor, imagem) VALUES('${nome}', ${valor}, '${imagem}')`;
+
+  conexao.query(sql, function(erro, retorno){
+    if(erro) throw erro;
+    
+    req.files.imagem.mv(__dirname + '/imagens/' + req.files.imagem.name);
+    console.log(retorno);
+  });
+
+
+  //retornar para a rota principal
+  res.redirect('/');
 })
 
 app.listen(port, ()=>{
